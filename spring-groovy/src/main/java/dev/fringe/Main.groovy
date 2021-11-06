4a8f3ec32495a9194a17e4ed354b2d04e0e4a663
livin <livin@DESKTOP-N91VEVT>
2021-11-06 22:24:25 +0900

    transaction

--- spring-groovy/src/main/java/dev/fringe/Main.groovy
+++ spring-groovy/src/main/java/dev/fringe/Main.groovy
@@ -6,20 +6,22 @@ import org.springframework.context.annotation.*
 import dev.fringe.config.MainConfig
 import dev.fringe.model.MainModel
 import dev.fringe.persistence.MainMapper
+import dev.fringe.service.MainService
 
 @Configuration
 @Import(value = MainConfig.class)
 public class Main implements InitializingBean {
 	
-	@Autowired MainMapper mapper;
+	@Autowired MainService mainService;
 	
 	static void main(String[] args) {
 		new AnnotationConfigApplicationContext(Main.class)
 	}
 	
 	public void afterPropertiesSet() throws Exception {
-		MainModel main = new MainModel(model: 'ss', type: 'sdsd');
+		MainModel main = new MainModel(model: '23', type: 'sdsd');
+		mainService.save(main)
 		println main
-		println mapper.select(main);
+//		println mapper.select(main);
 	}
 }
---
+++ spring-groovy/src/main/java/dev/fringe/dao/MainDao.groovy
@@ -0,0 +1,24 @@
+package dev.fringe.dao
+
+import org.hibernate.Session
+import org.hibernate.SessionFactory
+import org.springframework.aop.ThrowsAdvice
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+
+import dev.fringe.model.MainModel
+import dev.fringe.persistence.MainMapper
+
+
+@Repository
+public class MainDao {
+	
+	@Autowired SessionFactory sessionFactory;
+	
+	public void save(MainModel main) {
+		sessionFactory.getCurrentSession().save(main);
+	}
+}
